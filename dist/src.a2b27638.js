3e43413d940d797dad0d5ca307fc5c3c31daa452
Matheus <matheus.barone@raisin.com>
2020-08-11 17:45:38 +0200

    Update Grid api

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -42413,7 +42413,8 @@ var MoviesList = function MoviesList(_ref) {
   var movies = _ref.movies;
   return /*#__PURE__*/_react.default.createElement(_Grid.default, {
     container: true,
-    spacing: 24
+    justify: "center",
+    spacing: 2
   }, movies.map(function (movie) {
     return /*#__PURE__*/_react.default.createElement(_Grid.default, {
       item: true,
@@ -43325,7 +43326,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61882" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59130" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/components/movie-list/index.js
+++ src/components/movie-list/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import MovieItem from '../movie-item';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import MovieItem from "../movie-item";
 
 const MoviesList = ({ movies }) => {
-	return (
-		<Grid container spacing={24}>
-			{movies.map(movie => (
-				<Grid item xs={12} md={6} key={movie.id}>
-					<MovieItem movie={movie} />
-				</Grid>
-			))}
-		</Grid>
-	);
+  return (
+    <Grid container justify="center" spacing={2}>
+      {movies.map((movie) => (
+        <Grid item xs={12} md={6} key={movie.id}>
+          <MovieItem movie={movie} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default MoviesList;
